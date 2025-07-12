4be73ef63eb3f2094ceab2e97b7bdf5097d4286b
Julieta BACHUR <jbachur@CNP.com.ar>
2025-07-12 11:31:47 -0300

    update aboutData and resume

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,10 +1,12 @@
 export const aboutData = {
   title: "About me",
   description1:
-    "Hello! My name is Julieta Bachur. I'm a frontend web developer based in Buenos Aires, Argentina.",
+    "Hello! My name is Julieta Bachur and I'm from Buenos Aires, Argentina.",
   description2:
-    "I am pursuing a degree in Certified Tech Developer - Frontend Specialist at Digital House.",
-    description3:"Currently I'm working as a frontend developer and UX-UI designer at CNP Seguros. I am also part of a digital lab where we are being trained in APIs, AI and data analytics with the goal of developing the skills needed to create innovations for the company.",
-    description4:"Some of my passions include art, pastry making, language and business. I also love travelling, watching movies and listening to music.",
-    image: 2,
+    "I am a Frontend Developer and UX/UI Designer with an end-to-end perspective: I contribute to digital solution design from user experience to technical implementation and workflow automation. I work with Angular, React, and ASP.NET Core to build scalable and accessible interfaces, and I design consistent, user-centered experiences using Adobe XD.",
+    description3:"Currently I'm working as UX Engineer and Workflow Automation Specialist at CNP Seguros. I'm one of the few specialists in N8N workflow automation in my team, where I design processes that significantly improve operational efficiency. I also play an active role in solution design across various projects, combining design, development, and automation.",
+    description4:"I have a C2 level of english certified by EF SET and I'm learning portuguese at the moment. ",
+    description5:"I'm a naturally curious person with a strong willingness to learn. I enjoy exploring new technologies, solving complex challenges, and continuously growing in this ever-evolving field.",
+    description6:"In my free time I love travelling, cooking, making excercise, watching movies and listening to music.",
+    image: 1,
 };
