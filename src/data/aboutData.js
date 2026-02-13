2c1eb1c6be0b27ff928f3c5906a5a54c3f34e42a
Julieta BACHUR <jbachur@CNP.com.ar>
2026-02-13 14:24:54 -0300

    add hours saved to aboutData

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,10 +3,10 @@ export const aboutData = {
   description1:
     "Hi! I’m Julieta Bachur, an Automation Engineer based in Buenos Aires, Argentina.",
   description2:
-    "I design, build and maintain production-grade automations using N8N, APIs and business logic, focusing on reducing manual work and improving operational efficiency. I work directly with end users to understand real needs, design effective solutions, and ensure reliable workflows in production environments.",
-    description3:"I currently work at CNP Seguros, where I’m a technical reference for workflow automation within the Digital Lab. My role involves full ownership of automation logic, integrations, monitoring and maintenance, as well as participating in solution design when needed.",
-    description4:"I have a background in frontend development and functional interface design, which I apply pragmatically to support technical solutions and rapid validation.",
-    description5:"I speak English (C2) and I’m currently learning Portuguese.",
-    //description6:"In my free time I love travelling, cooking, making excercise, watching movies and listening to music.",
+    "I design, build and maintain production-grade automations using N8N, APIs and business logic, focusing on reducing manual work and improving operational efficiency. The workflows I’ve implemented have collectively saved over 1,700 hours of manual work per year, improving reliability and data accuracy across critical business processes.",
+    description3:"I work directly with end users to understand real needs, design effective solutions, and ensure stable, scalable workflows in production environments.",
+    description4:"I currently work at CNP Seguros, where I’m a technical reference for workflow automation within the Digital Lab. My role involves full ownership of automation logic, integrations, monitoring and maintenance, as well as participating in solution design when required.",
+    description5:"I have a background in frontend development and functional interface design, which I apply pragmatically to support technical solutions and rapid validation.",
+    description6:"I speak English (C2) and I’m currently learning Portuguese.",
     image: 1,
 };
